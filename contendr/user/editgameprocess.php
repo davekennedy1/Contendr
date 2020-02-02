576a3d3ae521cc720a80c2ce72c37869250988e5
Dave  Kennedy <dave.a.kennedy@gmail.com>
2020-02-02 22:07:00 +0000

    edit/delete game functionality

---
+++ contendr/user/editgameprocess.php
@@ -0,0 +1,127 @@
+<?php
+  session_start();
+  if (!isset($_SESSION['13072064_contendrUnom'])) {
+      header('Location: ../index.php');
+  }
+  $sessionUser = $_SESSION['13072064_contendrUnom'];
+  $uid = $_SESSION['13072064_contenderUID'];
+  include('../conn.php');
+
+  $sportType = "SELECT Sps_SportType.SportTypeID, Sps_SportType.SportTypeName FROM Sps_SportType";
+  $sportTypeResult = $conn->query($sportType);
+  $sportTypeNum = $sportTypeResult->num_rows;
+
+  $indoorID;
+  $outdoorID;
+
+  $gameType = "SELECT Sps_GameType.GameTypeID, Sps_GameType.GameTypeName FROM Sps_GameType";
+  $gameTypeResult = $conn->query($gameType);
+  $gameTypeNum = $gameTypeResult->num_rows;
+
+  $privateID;
+  $publicID;
+
+  $matchID = $conn->real_escape_string(trim($_POST['matchID']));
+  $public = $conn->real_escape_string(trim($_POST['public']));
+  $private = $conn->real_escape_string(trim($_POST['private']));
+  $publicOrPrivate;
+
+  if (!$gameTypeResult) {
+      echo $conn->error;
+  } else {
+      while ($row = $gameTypeResult->fetch_assoc()) {
+        $gameTypeID = $row['GameTypeID'];
+        $gameTypeName = $row['GameTypeName'];
+        if($gameTypeName == 'Private') {
+          $privateID = $gameTypeID;
+        } else if ($gameTypeName == 'Public'){
+          $publicID = $gameTypeID;
+        }
+      }
+  }
+
+  if($private == 'on'){
+    $publicOrPrivate = $privateID;
+  } else {
+    $publicOrPrivate = $publicID;
+  }
+
+  $venue = $conn->real_escape_string(trim($_POST['venue']));
+  $indoorOutdoor = $conn->real_escape_string(trim($_POST['indoorOutdoor']));
+
+  if (!$sportTypeResult) {
+      echo $conn->error;
+  } else {
+      while ($row = $sportTypeResult->fetch_assoc()) {
+        $sportTypeID = $row['SportTypeID'];
+        $sportTypeName = $row['SportTypeName'];
+        if($sportTypeName == 'Indoor') {
+          $indoorID = $sportTypeID;
+        } else if ($sportTypeName == 'Outdoor'){
+          $outdoorID = $sportTypeID;
+        }
+      }
+  }
+
+  if($indoorOutdoor == 'Indoor') {
+    $indoorOutdoor = $indoorID;
+  } else {
+    $indoorOutdoor = $outdoorID;
+  }
+
+  $matchType = "SELECT Sps_MatchType.MatchTypeID, Sps_MatchType.MatchTypeName FROM Sps_MatchType";
+  $matchTypeResult = $conn->query($matchType);
+  $matchTypeNum = $matchTypeResult->num_rows;
+  $teamGameID;
+  $individualsGameID;
+
+  if (!$matchTypeResult) {
+      echo $conn->error;
+  } else {
+      while ($row = $matchTypeResult->fetch_assoc()) {
+        $matchTypeID = $row['MatchTypeID'];
+        $matchTypeName = $row['MatchTypeName'];
+        if($matchTypeName == 'Team game') {
+          $teamGameID = $matchTypeID;
+        } else if ($matchTypeName == 'Individuals game'){
+          $individualsGameID = $matchTypeID;
+        }
+      }
+  }
+
+  $minPlayers = $conn->real_escape_string(trim($_POST['minPlayers']));
+  if($minPlayers > 2) {
+    $matchType = $teamGameID;
+  } else {
+    $matchType = $individualsGameID;
+  }
+  $sport = $conn->real_escape_string(trim($_POST['sport']));
+  $difficulty = $conn->real_escape_string(trim($_POST['difficulty']));
+  $date = $conn->real_escape_string(trim($_POST['date']));
+  $startTime = $conn->real_escape_string(trim($_POST['startTime']));
+  $date = $date.$startTime;
+  $dateChange=strtotime($date);
+  $matchDateTime = date('Y-m-d H:i:s',$dateChange);
+
+  $gameName = $conn->real_escape_string(trim($_POST['gameName']));
+  $cost = $conn->real_escape_string(trim($_POST['cost']));
+  $maxPlayers = $conn->real_escape_string(trim($_POST['maxPlayers']));
+
+  $status = $conn->real_escape_string(trim($_POST['status']));
+  $frequency = $conn->real_escape_string(trim($_POST['frequency']));
+  $end = $conn->real_escape_string(trim($_POST['endTime']));
+  $endToDate = strtotime($end);
+  $endTime = date('H:i:s', $endToDate);
+  $city = $conn->real_escape_string(trim($_POST['city']));
+
+  $updateGame = "UPDATE Sps_Match SET VenueID='$venue', SportTypeID='$indoorOutdoor', MatchTypeID='$matchType', SportID='$sport', ProficiencyLevelID='$difficulty', MatchDateTime='$matchDateTime', GameTypeID='$publicOrPrivate', MatchName='$gameName', Cost='$cost', MaxPlayers='$maxPlayers', MinPlayers='$minPlayers', MatchStatusID='$status', RecurringMatchID='$frequency', MatchEndTime='$endTime', CityID='$city' WHERE Sps_Match.MatchID = $matchID";
+
+  if ($conn->query($updateGame) === TRUE) {
+    echo "Record updated successfully";
+  } else {
+    echo "Error updating record: " . $conn->error;
+  }
+
+  header('Location: mygames.php');
+
+?>
